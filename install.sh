c377d0c5a4b506d785fc744bc1b5276bdc4fb93d
Samyak Puri <samyakpuri@gmail.com>
2018-11-04 17:46:01 +0100

    Fixed errors with install script

--- install.sh
+++ install.sh
@@ -2,14 +2,15 @@
 # File              : install.sh
 # Author            : Samyak Puri <samyakpuri@hotmail.com>
 # Date              : 18.10.2018
-# Last Modified Date: 18.10.2018
+# Last Modified Date: 04.11.2018
 # Last Modified By  : Samyak Puri <samyakpuri@hotmail.com>
 
 check(){
-    [[ -d ~/.dotfiles ]] && echo "Dotfiles already installed ...... exiting......." && exit
+    [[ -e ~/.dotfiles/.installed ]] && echo "Dotfiles already installed ...... exiting......." && exit
 
-    cd $HOME
-    git clone git@github.com:samyakpuri/dotfiles ~/.dotfiles
+    [[ ! -d ~/.dotfiles ]] && cd $HOME && git clone https://github.com/samyakpuri/dotfiles ~/.dotfiles
+   
+    echo "$HOME"
     cd "$HOME/.dotfiles"
 }
 
@@ -24,20 +25,22 @@ backup() {
 }
 
 makelinks(){
+    prefix=""	
+    [[ $3 = "d" ]] && prefix="."
     for name in $(ls $1); do
-        target="$2/.$name"
+        target="$2/$prefix$name"
         if [[ ! "$name" = "config" ]] && [[ ! "$name" = "README.MD" ]] && [[ ! "$name" = "gitconfig" ]]; then
            backup $target
            echo "-----> Symlinking your new $target"
-           ln -s "$sour/$name" "$target"
+           ln -snf "$1/$name" "$target"
         fi
     done
 }
 
 setupgit(){
 
-    cp gitconfig "$HOME/gitconfig"
-    echo "Type in your first and last name (no accent or special characters - e.g. 'ç'): "
+    cp gitconfig "$HOME"/.gitconfig
+    echo "Type in your first and last name (no accent or special characters - e.g. 'cs'): "
     read full_name
 
     echo "Type in your email address (the one used for your GitHub account): "
@@ -45,6 +48,13 @@ setupgit(){
 
     git config --global user.email $email
     git config --global user.name $full_name
+}
+
+finalize(){
+    ln -s "$PWD"/vim/vimrc "$HOME"/.vimrc
+    mkdir "$HOME"/.config/neovim
+    ln -s "$PWD"/vim/vimrc "$HOME"/.config/neovim/init.vim
+    mkdir -p "$HOME"/.backups/{backups,swaps,undofiles}
 }
 
 uninstall(){
@@ -58,7 +68,7 @@ uninstall(){
 
 if [[ "$1" = "un" ]]; then
     uninstall $HOME
-    uninstall "$HOME/.config"
+    uninstall "$HOME"/.config
     exit
 fi
 
@@ -66,10 +76,15 @@ fi
 check
 
 # Install the dotfiles
-makelinks "$PWD" "$HOME"
+makelinks "$PWD" "$HOME" "d"
 
-makelinks "$PWD/config" "$HOME/.config"
+makelinks "$PWD"/config "$HOME"/.config
 
 # Setup gitconfig
 setupgit
 
+# Finalize config
+finalize
+
+# Set install file
+touch "$HOME"/.dotfiles/.installed
