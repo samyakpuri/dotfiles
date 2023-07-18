6c77c0da39d006aa2072938987f75d39932cdbbd
Samyak Puri <samyakpuri@gmail.com>
2023-07-18 22:21:50 +0200

    updated

---
+++ config/nvim/lua/plugins/colorizer.lua
@@ -0,0 +1,4 @@
+return {
+  "lilydjwg/colorizer",
+  version = false,
+}
---
+++ config/nvim/lua/plugins/notify.lua
@@ -0,0 +1,8 @@
+return {
+  "rcarriga/nvim-notify",
+  setup = {
+    require("notify").setup({
+      background_colour = "#000000",
+    }),
+  },
+}
